f9b9bd6a95ce31c487dc83046be90ecee368ff66
Gazi-Md-Salauddin <mdsalauddin329132@gmail.com>
2026-07-10 11:26:27 +0600

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center bg-zinc-50 font-sans">
+      <main className="w-full max-w-3xl">
+        <h2>Home page</h2>
+      </main>
+    </div>
+  );
+}
